f8b0243319c12a97b127bc74e5229cab6874f27c
AntonRich <adamschelenberg@gmail.com>
2019-09-07 15:23:44 +0300

    02. Ecto chapter. Finished migrated before building forms.

--- lib/tumbl/accounts.ex
+++ lib/tumbl/accounts.ex
@@ -1,23 +1,21 @@
 defmodule Tumbl.Accounts do
   alias Tumbl.Accounts.User
-
-  def list_users do
-    [
-      %User{id: "1", name: "Jose", username: "josevalim"},
-      %User{id: "2", name: "Whatever", username: "whatever"},
-      %User{id: "3", name: "Pinky", username: "pink"}
-    ]
-  end
+  alias Tumbl.Repo
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id  end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} ->
-        Map.get(map, key) == val
-      end)
-    end)
+    Repo.get_by(User, params)
   end
+
+  def list_users do
+    Repo.all(User)
+  end
+
 end
